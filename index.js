a2623e54cf24f69300e4bb409744e787b0a60950
ChitrangdaPandya <81518376+ChitrangdaPandya@users.noreply.github.com>
2021-08-30 16:42:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,37 +4,37 @@ function nandleClick(){
   switch (buttonletters) {
 
       case "w":
-      var audio=new Audio("sounds/crash.mp3");
+      var audio=new Audio("crash.mp3");
       audio.play();
 
         break;
         case "a":
-        var audio=new Audio("sounds/kick-bass.mp3");
+        var audio=new Audio("kick-bass.mp3");
         audio.play();
 
           break;
           case "s":
-          var audio=new Audio("sounds/snare.mp3");
+          var audio=new Audio("snare.mp3");
           audio.play();
 
             break;
             case "d":
-            var audio=new Audio("sounds/tom-1.mp3");
+            var audio=new Audio("tom-1.mp3");
             audio.play();
 
               break;
               case "j":
-              var audio=new Audio("sounds/tom-2.mp3");
+              var audio=new Audio("tom-2.mp3");
               audio.play();
 
                 break;
                 case "k":
-                var audio=new Audio("sounds/tom-3.mp3");
+                var audio=new Audio("tom-3.mp3");
                 audio.play();
 
                   break;
                   case "l":
-                  var audio=new Audio("sounds/tom-4.mp3");
+                  var audio=new Audio("tom-4.mp3");
                   audio.play();
 
                     break;
@@ -56,37 +56,37 @@ function keyboard(event){
   switch (event.key) {
 
       case "w":
-      var audio=new Audio("sounds/crash.mp3");
+      var audio=new Audio("crash.mp3");
       audio.play();
 
         break;
         case "a":
-        var audio=new Audio("sounds/kick-bass.mp3");
+        var audio=new Audio("kick-bass.mp3");
         audio.play();
 
           break;
           case "s":
-          var audio=new Audio("sounds/snare.mp3");
+          var audio=new Audio("snare.mp3");
           audio.play();
 
             break;
             case "d":
-            var audio=new Audio("sounds/tom-1.mp3");
+            var audio=new Audio("tom-1.mp3");
             audio.play();
 
               break;
               case "j":
-              var audio=new Audio("sounds/tom-2.mp3");
+              var audio=new Audio("tom-2.mp3");
               audio.play();
 
                 break;
                 case "k":
-                var audio=new Audio("sounds/tom-3.mp3");
+                var audio=new Audio("tom-3.mp3");
                 audio.play();
 
                   break;
                   case "l":
-                  var audio=new Audio("sounds/tom-4.mp3");
+                  var audio=new Audio("tom-4.mp3");
                   audio.play();
 
                     break;
